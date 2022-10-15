7e3b84e380de4127a02608ac2557e9f3e01f4078
key-door <101494707+key-door@users.noreply.github.com>
2022-10-15 14:38:12 +0900

    commit

--- client.c
+++ client.c
@@ -6,13 +6,24 @@
 /*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 09:32:04 by keys              #+#    #+#             */
-/*   Updated: 2022/10/15 09:54:38 by keys             ###   ########.fr       */
+/*   Updated: 2022/10/15 14:37:57 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include<signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void ()
+{
+
+}
 int main(int argc, char **argv)
 {
-	if( argc!= 3 || !argv[2] )
+	if( argc != 3 || !argv[2] )
 		return 1;
+
+	signal(SIGUSR1, signal_handler);
 	return 0;
 }
