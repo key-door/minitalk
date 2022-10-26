0a79446170397447dbb55ef3ae05c2ec9cc68e16
key-door <101494707+key-door@users.noreply.github.com>
2022-10-26 23:07:16 +0900

    commit

--- client.c
+++ client.c
@@ -3,26 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 09:32:04 by keys              #+#    #+#             */
-/*   Updated: 2022/10/26 02:26:29 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/10/26 22:44:03 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+//sigusr1 sigusr2
+//http://uralowl.my.coocan.jp/unix/job/UNIX/kernel/signal.html
 
 static void	ft_kill(int pid, char c)
 {
 	int	i;
 
 	i = 8;
+	//ビット文字列データ
+	//https://www.microfocus.co.jp/manuals/ED60/VS2019/BKPFPFDATABITSTRING.html
+	// 1 => 0000 0001     6 => 0000 0110
 	while (i--)
 	{
+		// >> 右シフト　<<　左シフト
+		// & AND。両方１のときに１、それ以外は0にする演算子
+		// 1001 1100
+		//
+		// 0000 0001
+		// 0000 0001
 		if (c >> i & 1)
 			kill(pid, SIGUSR1);
+
 		else
 			kill(pid, SIGUSR2);
+		//マイクロ秒単位で実行を延期する
+		//https://linuxjm.osdn.jp/html/LDP_man-pages/man3/usleep.3.html
 		usleep(1000);
 	}
 }
@@ -40,6 +54,10 @@ int	main(int argc, char **argv)
 {
 	if (argc != 3 || !argv[2])
 		return (1);
+	//kill - プロセスに対して，シグナルを送る．
+	//https://linuxjm.osdn.jp/html/LDP_man-pages/man2/kill.2.html
+	//sig が 0 の場合、kill() はエラー検査を行います.
+	//sig = 0 の場合シグナルは送信されない．pid 引数が正しいかどうかチェックする
 	if (kill(ft_atoi(argv[1]), 0) == -1)
 		return (1);
 	ft_signal_handler(ft_atoi(argv[1]), argv[2]);
--- server.c
+++ server.c
@@ -3,14 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kyoda <kyoda@student.42tokyo.jp>           +#+  +:+       +#+        */
+/*   By: keys <keys@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 16:55:50 by keys              #+#    #+#             */
-/*   Updated: 2022/10/26 02:26:32 by kyoda            ###   ########.fr       */
+/*   Updated: 2022/10/26 23:06:31 by keys             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
+
+// struct sigaction {
+//     void     (*sa_handler)(int);
+//     void     (*sa_sigaction)(int, siginfo_t *, void *);
+//     sigset_t   sa_mask;
+//     int        sa_flags;
+//     void     (*sa_restorer)(void); };
 
 static void	get_signal(int sig)
 {
@@ -31,6 +38,8 @@ static void	get_signal(int sig)
 
 int	main(void)
 {
+	//sigaction - シグナルの動作の確認と変更
+	//https://linuxjm.osdn.jp/html/LDP_man-pages/man2/sigaction.2.html
 	struct sigaction	s_sa;
 	pid_t				pid;
 
@@ -40,9 +49,14 @@ int	main(void)
 	ft_putstr_fd("PID: ", 1);
 	ft_putnbr_fd(pid, 1);
 	ft_putstr_fd("\n", 1);
+	//sigemptyset() は set で与えられたシグナル集合を空に初期化.
+	//https://linuxjm.osdn.jp/html/LDP_man-pages/man3/sigsetops.3.html
 	sigemptyset(&s_sa.sa_mask);
+	//関数をセットする
 	s_sa.sa_handler = get_signal;
+	//flag は使わないが，何もなければ0をセット．しなくてもよい．
 	s_sa.sa_flags = 0;
+	//sigaction, rt_sigaction - シグナルの動作の確認と変更
 	sigaction(SIGUSR1, &s_sa, NULL);
 	sigaction(SIGUSR2, &s_sa, NULL);
 	while (1)
